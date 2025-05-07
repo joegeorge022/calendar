4db584143958fe9f6b5ce8096edff2e0a2e64d06
Joe George <127773439+joegeorge022@users.noreply.github.com>
2025-05-07 14:13:54 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    // i love unsplash avarude images ellam high quality aan pinne copyright free aan.
+  },
+};
+
+export default nextConfig;
