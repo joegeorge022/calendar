47697ab073e297230db2205b78a4a927273f6b2a
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-05-11 16:59:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
-    // i love unsplash avarude images ellam high quality aan pinne copyright free aan.
   },
 };
 
